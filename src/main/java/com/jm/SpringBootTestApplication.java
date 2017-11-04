2924ca29669ab8a0c49ba0a1cd7c5ba4726dd417
jiangjingming <baili@maihaoche.com>
2017-11-04 20:11:07 +0800

    代码初始化

--- src/main/java/com/jm/SpringBootTestApplication.java
+++ src/main/java/com/jm/SpringBootTestApplication.java
@@ -1,11 +1,13 @@
 package com.jm;
 
+import cn.jjm.annotation.EnableMonitorConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author jiangjingming
  */
+@EnableMonitorConfiguration
 @SpringBootApplication
 public class SpringBootTestApplication {
 
--- src/main/java/com/jm/controller/EsController.java
+++ src/main/java/com/jm/controller/EsController.java
@@ -1,18 +1,36 @@
 package com.jm.controller;
 
-import com.mhc.ambulance.annotation.ExceptionNotice;
+import cn.jjm.annotation.AccessMonitor;
+import cn.jjm.service.AccessMonitorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by jiangjingming on 2017/10/26.
  */
-@RestController("/es")
+@RestController
 public class EsController {
 
-    @RequestMapping("/find")
-    @ExceptionNotice
+    @Autowired
+    private AccessMonitorService accessMonitorService;
+
+
+    @RequestMapping("/baili")
+    @AccessMonitor(methodName = "baili")
     public Object find() {
         return "hello word";
+    }
+
+    @RequestMapping(value = "/findbaili")
+    public Object findtest() {
+        return accessMonitorService.findMethodName("baili");
+    }
+
+    @RequestMapping(value = "/find")
+    @AccessMonitor(methodName = "find")
+    public Object findShow(@RequestParam String methodName) {
+        return accessMonitorService.findMethodName(methodName);
     }
 }
